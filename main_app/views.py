c7de100581709ecc6e7bf5d25a1bdaeed40a63c0
mohdswar9@gmail.com <mohdswar9@gmail.com>
2025-03-16 14:53:15 +0300

    fick this shit

--- main_app/views.py
+++ main_app/views.py
@@ -7,8 +7,7 @@ from django.http import HttpResponse
 
 # Define the home view function
 def home(request):
-    # Send a simple HTML response
-    return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+   return render(request, 'index.html')
 
 def about(request):
-    return render(request, 'about.html')  
+    return render(request, 'about.html')
