762dcb650c051b9739cb0569566320195137b63b
mohdswar9@gmail.com <mohdswar9@gmail.com>
2025-03-16 14:49:59 +0300

    fuck this shit

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -29,8 +29,9 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
+    
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
