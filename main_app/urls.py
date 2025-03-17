a1a120c2e715acd5f28489f9791459584c6f27c2
mohdswar9@gmail.com <mohdswar9@gmail.com>
2025-03-17 22:45:47 +0300

    just a commit

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls  import path
 from  . import views
-
-urlpatterns =  [
-    path('',  views.home, name='home'),
-    path('about/',  views.about, name='about'),
+urlpatterns = [
+  path('', views.home, name='home'),
+  path('about/', views.about, name='about'),
+  path('cats/', views.cat_index, name='cat-index'),
 ]
